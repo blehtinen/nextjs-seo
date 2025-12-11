8ce4b839be5f3ac9cab6d8b5dc082917bd2bb051
blehtinen <barrylehtinen@gmail.com>
2025-12-11 23:34:36 +0000

    switch next.config back to js

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,22 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 
 
-// const nextConfig = {
-//     // experimental: {
-//     //     appDir: true,
-//     // },
-//     // output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
-//     // reactStrictMode: true,
-//     // swcMinify: true
-// }
+const nextConfig = {
+    // experimental: {
+    //     appDir: true,
+    // },
+    // output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+    // reactStrictMode: true,
+    // swcMinify: true
+}
 
-// //const { withContentlayer } = require("next-contentlayer")
+//const { withContentlayer } = require("next-contentlayer")
 
-// module.exports = nextConfig;// withContentlayer(nextConfig)
-import type { NextConfig } from "next";
+module.exports = nextConfig;// withContentlayer(nextConfig)
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
