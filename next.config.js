c813fcf103da30a64ceac2c631282514636b928c
blehtinen <barrylehtinen@gmail.com>
2025-12-11 23:44:42 +0000

    next.config changed back to js...for real

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+
+const nextConfig = {
+    // experimental: {
+    //     appDir: true,
+    // },
+    // output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+    // reactStrictMode: true,
+    // swcMinify: true
+}
+
+module.exports = nextConfig;
