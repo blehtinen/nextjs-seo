52b9090baa0fc80ec92142b61405c6cb97b9a6ab
blehtinen <barrylehtinen@gmail.com>
2025-12-11 22:57:22 +0000

    renamed for files

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+// /** @type {import('next').NextConfig} */
+
+
+// const nextConfig = {
+//     // experimental: {
+//     //     appDir: true,
+//     // },
+//     // output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+//     // reactStrictMode: true,
+//     // swcMinify: true
+// }
+
+// //const { withContentlayer } = require("next-contentlayer")
+
+// module.exports = nextConfig;// withContentlayer(nextConfig)
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
